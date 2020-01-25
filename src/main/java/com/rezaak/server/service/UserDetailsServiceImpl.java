4c53526604ddf10eb03964a317f4514568fdf175
rzjrh <rzjrh@hotmail.com>
2020-01-24 22:45:13 -0600

    UserServiceDetails- Security layer

---
+++ src/main/java/com/rezaak/server/service/UserDetailsServiceImpl.java
@@ -0,0 +1,40 @@
+package com.rezaak.server.service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.rezaak.server.model.User;
+import com.rezaak.server.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user= userRepository.findByUsername(username).orElse(null);
+		if (user == null) {
+			throw new UsernameNotFoundException(username);
+		}
+		Set <GrantedAuthority> grantedAuthorities = new HashSet<>();
+		grantedAuthorities.add(new SimpleGrantedAuthority(user.getRole().name()));
+		return new org.springframework.security.core.userdetails.User(
+			user.getUsername(),
+			user.getPassword(),
+			grantedAuthorities
+		);
+	}
+
+	
+	
+}
